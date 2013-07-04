ec2fd636fcd87bda8b291e7e7695bedc44b2697f
todor balabanov <todor.balabanov@gmail.com>
2013-07-04 10:39:26 +0300

    In Card class first color conversion function implemented.

--- src/eu/veldsoft/marias/Card.java
+++ src/eu/veldsoft/marias/Card.java
@@ -1,17 +1,68 @@
 package eu.veldsoft.marias;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Class for playing card representation.
+ * 
+ * @author Todor Balabanov
+ * 
+ * @email tdb@tbsoft.eu
+ * 
+ * @date 04 Jul 2013
+ */
 class Card {
+	private final static Logger LOGGER = Logger.getLogger(Card.class.getName());
+
 	public Card() {
+		LOGGER.setLevel(Level.INFO);
 	}
 
+	/**
+	 * Convert integer constant of the color to string representation.
+	 * 
+	 * @param c
+	 *            Integer constant.
+	 * 
+	 * @return String representation of the suit.
+	 * 
+	 * @author Todor Balabanov
+	 * 
+	 * @email tdb@tbsoft.eu
+	 * 
+	 * @date 04 Jul 2013
+	 */
 	public static String color(int c) {
-		return (null);
+		if (c / 8 == 0) {
+			return "gula";
+		}
+
+		if (c / 8 == 1) {
+			return "zalud";
+		}
+
+		if (c / 8 == 2) {
+			return "zelen";
+		}
+
+		if (c / 8 == 3) {
+			return "cerven";
+		}
+
+		/*
+		 * This line is used for debug only.
+		 */
+		LOGGER.info("zly kod karty: " + c);
+
+		return "";
 	}
 
 	/**
 	 * slovny zaklad
 	 */
 	public static String colorZ(int c) {
+		// TODO To be done by Miro.
 		return (null);
 	}
 
