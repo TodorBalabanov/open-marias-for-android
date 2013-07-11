47b22011f27fbcc24d7d24b70faeb0beec55ec49
Todor Balabanov <todor.balabanov@gmail.com>
2013-07-11 23:54:33 +0300

    Some methods were implemented.

--- src/eu/veldsoft/marias/Stav.java
+++ src/eu/veldsoft/marias/Stav.java
@@ -1,12 +1,28 @@
 package eu.veldsoft.marias;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Class Stav keeps informations on game state. Also keeps information on game
  * goals in instance of Hra.
+ *
+ * @author Todor Balabanov
+ *
+ * @email tdb@tbsoft.eu
+ *
+ * @date 11 Jul 2013
  */
 class Stav {
+	/**
+	 * Logger for debug.
+	 */
+	private final static Logger LOGGER = Logger.getLogger(Stav.class.getName());
+	static {
+		LOGGER.setLevel(Level.INFO);
+	}
 
 	/**
 	 * id of player, who's turn it is now. Id is to list of players Game
@@ -45,17 +61,17 @@ class Stav {
 	/**
 	 * kopa has 0-2 cards
 	 */
-	public List<Integer> kopa;
+	public List<Integer> kopa = new ArrayList<Integer>();
 
 	/**
 	 * cHist contains card history. Kopa is suffix of cHist.
 	 */
-	public List<Integer> cHist;
+	public List<Integer> cHist = new ArrayList<Integer>();
 
 	/**
 	 * pHist[i] is player that won i-th stich
 	 */
-	public List<Integer> pHist;
+	public List<Integer> pHist = new ArrayList<Integer>();
 
 	/**
 	 * results
@@ -109,19 +125,111 @@ class Stav {
 	}
 
 	/**
-	 * Checks if this state is valid
+	 * Checks if this state is valid.
 	 * 
+	 * @return Message for valid state.
+	 *
+	 * @author Todor Balabanov
+	 *
+	 * @email tdb@tbsoft.eu
+	 *
+	 * @date 11 Jul 2013
 	 */
 	public String validate() {
-		return (null);
+		if(kopa.size()<0 || kopa.size()>3) {
+			return ("Na kope je zly pocet kariet: "  + kopa.size());
+		}
+
+		for(int c=0;c<32;c++){
+			/*
+			 * Check if the card can be found more than once in the history.
+			 */
+			if(cHist.indexOf(c)!=cHist.lastIndexOf(c)){
+				//TODO This kind of checking can be wrong.
+				return ("Duplicitna karta v historii: " + c);
+			}
+		}
+
+		if(kopa.size()>cHist.size()) {
+			return ("Na kope je viac kariet ako v historii");
+		}
+
+		for(int i=1;i<=kopa.size();i++) {
+			if(cHist.get(cHist.size()-i) != kopa.get(kopa.size()-i)){
+				return ("Karty na kope nesedia s kartami v historii");
+			}
+		}
+
+		return(""); 
 	}
 
 	/**
 	 * Returns id of player that takes the trick. Also modifies the state.
 	 * 
+	 * @return Player id.
+	 *
+	 * @author Todor Balabanov
+	 *
+	 * @email tdb@tbsoft.eu
+	 *
+	 * @date 11 Jul 2013
 	 */
 	public int trick() {
-		return (0);
+		if(kopa.size()!=3){
+			LOGGER.info("Stav::trick(): Kopa neni plna");
+			return( -1);
+		}
+
+		if(pHist.isEmpty() && id!=forhont){
+			LOGGER.info("Stav::trick(): Nesedi id (prvy stich)");
+			return( -1);
+		}
+
+		//TODO This check for the last element can be wrong in Java.
+		if(pHist.isEmpty()==false && pHist.lastIndexOf(id)!=(pHist.size()-1)){
+			LOGGER.info("Stav::trick(): Nesedi id s pHist");
+			return( -1);
+		}
+
+		if(id!=vysid){
+			LOGGER.info("Stav::trick(): Nesedi id s vysid");
+			return( -1);
+		}
+
+		int c=cHist.get(cHist.size()-3);
+		int d=cHist.get(cHist.size()-2);
+		int e=cHist.get(cHist.size()-1);
+
+		/*
+		 * Player that won the trick.
+		 */
+		int p=-1;
+
+		if(!Card.stronger(d,c,hra) && !Card.stronger(e,c,hra)){
+			p=vysid;
+		}else if(Card.stronger(d,c,hra) && !Card.stronger(e,d,hra)){
+			p=(vysid+1)%3;
+		}else{
+			p=(vysid+2)%3;
+		}
+
+		int stichPoints=0;
+
+		if(Card.isFatty(c)){stichPoints+=10;}
+
+		if(Card.isFatty(d)){stichPoints+=10;}
+
+		if(Card.isFatty(e)){stichPoints+=10;}
+
+		if(kolo==9){stichPoints+=10;}
+
+		if(p==forhont){
+			hra.forhontPoints += stichPoints;
+		}else{
+			hra.oppPoints +=stichPoints;
+		}
+
+		return( p );
 	}
 
 	/**
@@ -184,3 +292,4 @@ class Stav {
 		return (0);
 	}
 }
+
