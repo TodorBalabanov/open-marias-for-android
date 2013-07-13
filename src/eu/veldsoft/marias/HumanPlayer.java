51b987e2b7b12a13d59626b7d9b4858a8bce091d
Todor Balabanov <todor.balabanov@gmail.com>
2013-07-14 00:37:39 +0300

    Work unit for Venci.

--- src/eu/veldsoft/marias/HumanPlayer.java
+++ src/eu/veldsoft/marias/HumanPlayer.java
@@ -1,14 +1,18 @@
 package eu.veldsoft.marias;
 
+//TODO To be done by Venci.
 class HumanPlayer extends Player {
 	public HumanPlayer() {
+		//TODO To be done by Venci.
 	}
 
 	public int play() {
+		//TODO To be done by Venci.
 		return (0);
 	}
 
 	public int bid() {
+		//TODO To be done by Venci.
 		return (0);
 	}
 }
