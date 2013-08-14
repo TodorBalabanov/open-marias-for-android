089ea639b5904a2825d51f513fb7176872cb9bbb
todor balabanov <todor.balabanov@gmail.com>
2013-08-14 14:09:40 +0300

    Some missing classes were added.

---
+++ src/eu/veldsoft/marias/CardItemGroup.java
@@ -0,0 +1,7 @@
+package eu.veldsoft.marias;
+
+public class CardItemGroup /* extends QObject, QGraphicsItemGroup */{
+	// Q_OBJECT
+	// Q_PROPERTY(QPointF pos READ pos WRITE setPos)
+	// Q_PROPERTY(qreal opacity READ opacity WRITE setOpacity)
+}
