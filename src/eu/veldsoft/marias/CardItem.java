36555bf3338d6588259802dcb3e82e55f6b29333
todor balabanov <todor.balabanov@gmail.com>
2013-08-14 14:09:40 +0300

    Some missing classes were added.

---
+++ src/eu/veldsoft/marias/CardItem.java
@@ -0,0 +1,18 @@
+package eu.veldsoft.marias;
+
+import android.view.MotionEvent;
+
+public class CardItem /* extends QObject, QGraphicsPixmapItem */{
+	// Q_OBJECT
+	// Q_PROPERTY(QPointF pos READ pos WRITE setPos)
+	// Q_PROPERTY(qreal x READ x WRITE setX)
+	// Q_PROPERTY(qreal y READ y WRITE setY)
+	// Q_PROPERTY(qreal opacity READ opacity WRITE setOpacity)
+	// Q_PROPERTY(qreal z READ zValue WRITE setZValue)
+
+	public int cid;
+
+	public void mousePressEvent(MotionEvent event) {
+		// TODO To be done by ...
+	}
+}
---
+++ src/eu/veldsoft/marias/SuperText.java
@@ -0,0 +1,6 @@
+package eu.veldsoft.marias;
+
+public class SuperText /* extends QGraphicsTextItem */{
+	// Q_OBJECT
+	// Q_PROPERTY(qreal opacity READ opacity WRITE setOpacity)
+}
