3954f059dd6fe47f973a09010942f2c8e29c870b
todor balabanov <todor.balabanov@gmail.com>
2013-08-14 16:16:31 +0300

    Few more class templates were added.

--- src/eu/veldsoft/marias/DeskView.java
+++ src/eu/veldsoft/marias/DeskView.java
@@ -5,269 +5,303 @@ import java.util.List;
 
 public class DeskView {
 
-		/**
-		 * instance
-		 */
-		private static DeskView instance;
+	/**
+	 * instance
+	 */
+	private static DeskView instance;
 
-		/**
+	/**
 		 * 
 		 */
-		//private QGraphicsScene graphicsScene;
-		
-		/**
+	// private QGraphicsScene graphicsScene;
+
+	/**
 		 * 
 		 */
-		//private QGraphicsView graphicsView;
-		
-		/**
+	// private QGraphicsView graphicsView;
+
+	/**
 		 * 
 		 */
-		//private QSignalMapper signalMapper;
-		
-		/**
-		 * Pointer to game
-		 */
-		private Game game;
-		
-		/**
-		 * Pointers to graphics items - cards. They are only moved from one position to another instead of creating and destroying.
-		 */
-		private CardItem cardItem[] = new CardItem[32];
-		
-		/**
-		 * Image of the card back.
-		 */
-		//private QPixmap rub;
-		
-		/** 
-		 * Images of cards.
-		 */
-		//private QPixmap images[32];
-		
-		/**
-		 * Tromf icons images.
-		 */
-		//private QPixmap suits[4];
-		
-		/**
-		 * Console output.
-		 */
-		private List<String> out = new ArrayList<String>();
-		
-		/**
-		 * positions in the graphics view for each of the 3 players:
-		 * stack
-		 */
-		//private QPoint poziciaKopy[3];
-		
-		/**
-		 * cards (upper left corner of the leftmost card)
-		 */
-		//private QPoint poziciaKariet[3];
-		
-		/**
-		 * tromf icon
-		 */
-		//private QPoint poziciaTromfu[3];
-		
-		/**
-		 * name label
-		 */
-		//private QPoint poziciaMena[3];
-		
-		/**
-		 * where stich(trick) goes when a players takes it
-		 */
-		//private QPoint poziciaStichov[3];
-		
-		/**
-		 * where supertext is animated
-		 */
-		//private QPoint poziciaTextu[3];
-		
-		/**
-		 * deck and then talon
-		 */
-		//private QPoint poziciaBase;
-		
-		/**
-		 * graphics items of forhont light
-		 */
-		//private QGraphicsEllipseItem* lightSimple[3];
-		
-		/**
+	// private QSignalMapper signalMapper;
+
+	/**
+	 * Pointer to game
+	 */
+	private Game game;
+
+	/**
+	 * Pointers to graphics items - cards. They are only moved from one position
+	 * to another instead of creating and destroying.
+	 */
+	private CardItem cardItem[] = new CardItem[32];
+
+	/**
+	 * Image of the card back.
+	 */
+	// private QPixmap rub;
+
+	/**
+	 * Images of cards.
+	 */
+	// private QPixmap images[32];
+
+	/**
+	 * Tromf icons images.
+	 */
+	// private QPixmap suits[4];
+
+	/**
+	 * Console output.
+	 */
+	private List<String> out = new ArrayList<String>();
+
+	/**
+	 * positions in the graphics view for each of the 3 players: stack
+	 */
+	// private QPoint poziciaKopy[3];
+
+	/**
+	 * cards (upper left corner of the leftmost card)
+	 */
+	// private QPoint poziciaKariet[3];
+
+	/**
+	 * tromf icon
+	 */
+	// private QPoint poziciaTromfu[3];
+
+	/**
+	 * name label
+	 */
+	// private QPoint poziciaMena[3];
+
+	/**
+	 * where stich(trick) goes when a players takes it
+	 */
+	// private QPoint poziciaStichov[3];
+
+	/**
+	 * where supertext is animated
+	 */
+	// private QPoint poziciaTextu[3];
+
+	/**
+	 * deck and then talon
+	 */
+	// private QPoint poziciaBase;
+
+	/**
+	 * graphics items of forhont light
+	 */
+	// private QGraphicsEllipseItem* lightSimple[3];
+
+	/**
 		 * 
 		 */
-		//private QGraphicsEllipseItem* lightGradient[3];
-		
-		/**
+	// private QGraphicsEllipseItem* lightGradient[3];
+
+	/**
 		 * 
 		 */
-		//private QSettings* settings;
-		
-		/**
+	// private QSettings* settings;
+
+	/**
 		 * 
 		 */
-		//private QFont font;
-		
-		public static DeskView Instance() {
-			//TODO To be done by ...
-			return(null);
-		}
-		
-//		/**
-//		  *Instance should be created only once. Now it is created by Marias class.
-//		  *It takes two arguments - ui of the marias for access to graphics view and game for access to state of the game and players
-//		  *
-//		  */
-//		public static void createInstance(Ui_Marias* ui,Game* g);
-//		
-//		/**
-//		  *Instance is created by Marias, so it is also destroyed only by Marias.
-//		  *
-//		  */
-//		public static void destroyInstance();
-//		
-//		/**
-//		  *Cleans graphics scene of graphics items, so they won't be stored in memory anymore.
-//		  *By default, cards and animations are not removed, because cards are not drawed, only moved, so they stay.
-//		  *Animations are removed after they are finished.
-//		  *
-//		  */
-//		public static void cleanGraphics(bool all=false);
-//		
-//		/**
-//		  *Gathers all cards from desk, cleans all animations if some remains and recreates the cards at deck.
-//		  *
-//		  */
-//		public static void gather();
-//		
-//		public static void intro();
-//		
-//		/**
-//		  *Moves card with id cid to position so it appears at the hand of player with id pid at position offset.
-//		  *It is used during card dealing and in case when a players plays a card and a gap arises in the hand - the cards are shifted then.
-//		  *Note: offset is zero-based.
-//		  *
-//		  */
-//		public static void rozdaj(int cid, int pid,int offset,int pause,bool last=false);
-//
-//		/**
-//		  *Talon is a special keyword of this game. Google translated it as "kitty", but it evokes me some kind of cat:)
-//		  *The forhont chooses 2 cards that other players don't see and he is not playing with that cards. After this, all players has exactly 10 cards.
-//		  *Talon consists of that 2 cards and is located at the position of the former card pile.
-//		  *cid is id of the card, forhont wants to give away.
-//		  *If the flag last is on, it means, the last card has been given to talon and the finish of the animation is connected to next action.
-//		  *
-//		  */
-//		public static void talon(int cid,bool last=false);
-//		
-//		/**
-//		  *Moves cards in player's hand to their correct positions.
-//		  *
-//		  */
-//		public static void fixHand(int pid,bool last=false);
-//		
-//		/**
-//		  *Tromf is also a special keyword, it is translated as "trump". It is a "chosen card" - chosen by forhont.
-//		  *The color(or suit) of the tromf will be stronger than other colors. At the start of the bidding, it is revealed to other players.
-//		  *Tromf is then displayed in front of forhont, so players can remember the chosen color.
-//		  *This function is only called after tromf has been chosen, so it only ejects the card softly to be visible.
-//		  *
-//		  */
-//		public static void ejectTromf(int cid,bool reverse=false);
-//
-//		/**
-//		  *Sets the card image. If the former was back of the card, now it will be front.
-//		  *
-//		  */
-//		public static void revealCard(int cid);
-//		
-//		/**
-//		  *Sets the card image to back. The value of the card will therefore be hidden.
-//		  *
-//		  */
-//		public static void hideCard(int cid);
-//
-//		/**
-//		  *Draws light at forhont, name labels with money, console with game messages and tromf suit.
-//		  *
-//		  */
-//		public static void draw();
-//		
-//		/**
-//		  *Cleans all the graphics and displays results. They are stored in game->stav.hra.results
-//		  *
-//		  */
-//		public static void drawResults();
-//		
-//		/**
-//		  *Animate card cid from player pid to the stack (poziciaKopy[pid])
-//		  *
-//		  */
-//		public static void animateCard(int cid,int pid);
-//		
-//		/**
-//		  *Animate stich(trick) - all 3 cards at the stack(kopa) are transferred to player pid.
-//		  *
-//		  */
-//		public static void animateStich(int pid);
-//		
-//		//deprecated
-//		public static void show();
-//		
-//		/**
-//		  *Dirty hook how anyone can easily get pointer to Game (Game have access to any class).
-//		  *
-//		  */
-//		public static Game* getGame();
-//		
-//		/**
-//		  *Prints animated text s that says player with id pos. Also logs it.
-//		  *
-//		  */
-//		public static void print(QString s, int pos=0);
-//		
-//		/**
-//		  *Adds given string to console output.
-//		  *
-//		  */
-//		public static void log(QString s);
-//		
-//		public DeskView(Ui_Marias* ui,Game* g);
-//
-//		public void _cleanGraphics(bool all=false);
-//
-//		public void _gather();
-//
-//		public void _intro();
-//
-//		public void _rozdaj(int cid, int pid,int offset,int pause,bool last=false);
-//
-//		public void _fixHand(int pid,bool last);
-//
-//		public void _talon(int cid,bool last);
-//
-//		public void _ejectTromf(int cid,bool reverse);
-//
-//		public void _revealCard(int cid);
-//
-//		public void _hideCard(int cid);
-//
-//		public void _draw();
-//
-//		public void _drawResults();
-//
-//		public void _animateCard(int cid,int pid);
-//
-//		public void _animateStich(int pid);
-//
-//		public void _show();
-//
-//		public Game* _getGame();
-//
-//		public void _print(QString s, int pos);
-//
-//		public void _log(QString s);
+	// private QFont font;
+
+	public static DeskView Instance() {
+		// TODO To be done by ...
+		return (null);
+	}
+
+	// TODO It is only for project to compile.
+	private class Ui_Marias {
+	}
+
+	/**
+	 * Instance should be created only once. Now it is created by Marias class.
+	 * It takes two arguments - ui of the marias for access to graphics view and
+	 * game for access to state of the game and players
+	 * 
+	 */
+	public static void createInstance(Ui_Marias ui, Game g) {
+	}
+
+	/**
+	 * Instance is created by Marias, so it is also destroyed only by Marias.
+	 * 
+	 */
+	public static void destroyInstance() {
+	}
+
+	// /**
+	// *Cleans graphics scene of graphics items, so they won't be stored in
+	// memory anymore.
+	// *By default, cards and animations are not removed, because cards are not
+	// drawed, only moved, so they stay.
+	// *Animations are removed after they are finished.
+	// *
+	// */
+	// public static void cleanGraphics(bool all=false);
+	//
+	// /**
+	// *Gathers all cards from desk, cleans all animations if some remains and
+	// recreates the cards at deck.
+	// *
+	// */
+	// public static void gather();
+	//
+	// public static void intro();
+	//
+	// /**
+	// *Moves card with id cid to position so it appears at the hand of player
+	// with id pid at position offset.
+	// *It is used during card dealing and in case when a players plays a card
+	// and a gap arises in the hand - the cards are shifted then.
+	// *Note: offset is zero-based.
+	// *
+	// */
+	// public static void rozdaj(int cid, int pid,int offset,int pause,bool
+	// last=false);
+	//
+	// /**
+	// *Talon is a special keyword of this game. Google translated it as
+	// "kitty", but it evokes me some kind of cat:)
+	// *The forhont chooses 2 cards that other players don't see and he is not
+	// playing with that cards. After this, all players has exactly 10 cards.
+	// *Talon consists of that 2 cards and is located at the position of the
+	// former card pile.
+	// *cid is id of the card, forhont wants to give away.
+	// *If the flag last is on, it means, the last card has been given to talon
+	// and the finish of the animation is connected to next action.
+	// *
+	// */
+	// public static void talon(int cid,bool last=false);
+	//
+	// /**
+	// *Moves cards in player's hand to their correct positions.
+	// *
+	// */
+	// public static void fixHand(int pid,bool last=false);
+	//
+	// /**
+	// *Tromf is also a special keyword, it is translated as "trump". It is a
+	// "chosen card" - chosen by forhont.
+	// *The color(or suit) of the tromf will be stronger than other colors. At
+	// the start of the bidding, it is revealed to other players.
+	// *Tromf is then displayed in front of forhont, so players can remember the
+	// chosen color.
+	// *This function is only called after tromf has been chosen, so it only
+	// ejects the card softly to be visible.
+	// *
+	// */
+	// public static void ejectTromf(int cid,bool reverse=false);
+	//
+	// /**
+	// *Sets the card image. If the former was back of the card, now it will be
+	// front.
+	// *
+	// */
+	// public static void revealCard(int cid);
+	//
+	// /**
+	// *Sets the card image to back. The value of the card will therefore be
+	// hidden.
+	// *
+	// */
+	// public static void hideCard(int cid);
+	//
+	// /**
+	// *Draws light at forhont, name labels with money, console with game
+	// messages and tromf suit.
+	// *
+	// */
+	// public static void draw();
+	//
+	// /**
+	// *Cleans all the graphics and displays results. They are stored in
+	// game->stav.hra.results
+	// *
+	// */
+	// public static void drawResults();
+	//
+	// /**
+	// *Animate card cid from player pid to the stack (poziciaKopy[pid])
+	// *
+	// */
+	// public static void animateCard(int cid,int pid);
+
+	/**
+	 * Animate stich(trick) - all 3 cards at the stack(kopa) are transferred to
+	 * player pid.
+	 * 
+	 */
+	public static void animateStich(int pid) {
+
+	}
+
+	// //deprecated
+	// public static void show();
+	//
+	// /**
+	// *Dirty hook how anyone can easily get pointer to Game (Game have access
+	// to any class).
+	// *
+	// */
+	// public static Game* getGame();
+	//
+	// /**
+	// *Prints animated text s that says player with id pos. Also logs it.
+	// *
+	// */
+	// public static void print(QString s, int pos=0);
+
+	/**
+	 * Adds given string to console output.
+	 * 
+	 */
+	public static void log(String s) {
+
+	}
+
+	// public DeskView(Ui_Marias* ui,Game* g);
+	//
+	// public void _cleanGraphics(bool all=false);
+	//
+	// public void _gather();
+	//
+	// public void _intro();
+	//
+	// public void _rozdaj(int cid, int pid,int offset,int pause,bool
+	// last=false);
+	//
+	// public void _fixHand(int pid,bool last);
+	//
+	// public void _talon(int cid,bool last);
+	//
+	// public void _ejectTromf(int cid,bool reverse);
+	//
+	// public void _revealCard(int cid);
+	//
+	// public void _hideCard(int cid);
+	//
+	// public void _draw();
+	//
+	// public void _drawResults();
+	//
+	// public void _animateCard(int cid,int pid);
+
+	public void _animateStich(int pid) {
+
+	}
+
+	// public void _show();
+	//
+	// public Game* _getGame();
+	//
+	// public void _print(QString s, int pos);
+	//
+	// public void _log(QString s);
 }
--- src/eu/veldsoft/marias/Game.java
+++ src/eu/veldsoft/marias/Game.java
@@ -17,195 +17,221 @@ import java.util.logging.Logger;
  * @data 14 Aug 2013
  */
 class Game {
-    /**
-     * Logger for debug.
-     */
-    private final static Logger LOGGER = Logger.getLogger(Game.class.getName());
-    static {
-        LOGGER.setLevel(Level.INFO);
-    }
+	/**
+	 * Logger for debug.
+	 */
+	private final static Logger LOGGER = Logger.getLogger(Game.class.getName());
+	static {
+		LOGGER.setLevel(Level.INFO);
+	}
+
+	// TODO Only dummy class.
+	private class Settings {
+		public static final int IniFormat = 0;
+
+		public Settings(String string, int format) {
+		}
+
+		public int value(String string, int value) {
+			return (0);
+		}
+
+		public Object value(String string, String string2) {
+			return ("");
+		}
+	}
 
 	// TODO Use shared preferences.
-	// public QSettings settings;
+	public Settings settings;
 
-    /**
-     * Players container.
-     */
+	/**
+	 * Players container.
+	 */
 	public List<Player> players;
 
-    /**
-     * Deck of cards.
-     */
+	/**
+	 * Deck of cards.
+	 */
 	public List<Integer> deck;
 
-    /**
-     * Current state of the game.
-     */
+	/**
+	 * Current state of the game.
+	 */
 	public Stav stav;
 
-    /**
-     * If is now the human turn flag.
-     */
+	/**
+	 * If is now the human turn flag.
+	 */
 	public boolean waitingForClick;
 
-    /**
+	/**
      *
      */
 	public BiddingDialog bd;
 
-    /**
-     * Marias game logic.
-     */
+	/**
+	 * Marias game logic.
+	 */
 	public Marias marias;
 
-    /**
-     * Quick game flag.
-     */
+	/**
+	 * Quick game flag.
+	 */
 	public boolean quickGame;
 
-    /**
-     * Profiler instance.
-     */
+	/**
+	 * Profiler instance.
+	 */
 	public Profiler profiler;
 
-    /**
-     * Pseudo-random number generator.
-     */
+	/**
+	 * Pseudo-random number generator.
+	 */
 	public Random rg;
 
-    /**
-     * for poeple test
-     */
+	/**
+	 * for poeple test
+	 */
 	public int stats[] = new int[3];
 
-    /**
-     * Constructor.
-     *
-     * @param m Marias object reference.
-     *
-     * @author Todor Balabanov
-     * @email tdb@tbsoft.eu
-     * @date 14 Aug 2013
-     */
+	/**
+	 * Constructor.
+	 * 
+	 * @param m
+	 *            Marias object reference.
+	 * 
+	 * @author Todor Balabanov
+	 * @email tdb@tbsoft.eu
+	 * @date 14 Aug 2013
+	 */
 	public Game(Marias m) {
-        //TODO Deep copy is bettter.
-        marias = m;
+		// TODO Deep copy is bettter.
+		marias = m;
 
-        //TODO Should be implemented because it is different than Qt implementation.
-        //settings = new Settings("marias.ini", QSettings::IniFormat);
+		// TODO Should be implemented because it is different than Qt
+		// implementation.
+		settings = new Settings("marias.ini", Settings.IniFormat);
 
-        bd = new BiddingDialog(this);
+		bd = new BiddingDialog(this);
 
-        profiler = new Profiler();
+		profiler = new Profiler();
 
-        for(int i=0;i<stats.length;i++) {
-            stats[i] = 0;
-        }
+		for (int i = 0; i < stats.length; i++) {
+			stats[i] = 0;
+		}
 	}
 
-    /**
-     * Initialize game.
-     *
-     *
-     * @author Todor Balabanov
-     * @email tdb@tbsoft.eu
-     * @date 14 Aug 2013
-     */
+	/**
+	 * Initialize game.
+	 * 
+	 * 
+	 * @author Todor Balabanov
+	 * @email tdb@tbsoft.eu
+	 * @date 14 Aug 2013
+	 */
 	public void init() {
-        LOGGER.info( "game init" );
-        quickGame = false;
+		LOGGER.info("game init");
+		quickGame = false;
 
-//TODO Settings should be available in order this fragmetn to work.
-//        if(settings.value("shuffling/random",1)==1){
-//            LOGGER.info("random");
-//            rg.setSeed();
-//        }else{
-//            LOGGER.info("not random");
-//            rg.setSeed(settings.value("shuffling/seed",47).toInt());
-//        }
-//
-//        shuffleDeck();
-//
-//        players.clear();
-//        players.add(PlayerFactory::create(settings.value("players/front","HumanPlayer").toString()));
-//        players.add(PlayerFactory::create(settings.value("players/left","RandomPlayer").toString()));
-//        players.add(PlayerFactory::create(settings.value("players/right","RandomPlayer").toString()));
-//        players[0].name = settings.value("players/front_name",tr("front")).toString();
-//        players[1].name = settings.value("players/left_name",tr("left")).toString();
-//        players[2].name = settings.value("players/right_name",tr("right")).toString();
+		// TODO Settings should be available in order this fragmetn to work.
+		if (settings.value("shuffling/random", 1) == 1) {
+			LOGGER.info("random");
+			rg.setSeed(System.currentTimeMillis());
+			;
+		} else {
+			LOGGER.info("not random");
+			rg.setSeed(settings.value("shuffling/seed", 47));
+		}
 
-        //TODO Use for-each loop.
-        for(int i=0;i<3;i++){
-            //TODO May be internal objects are unchainged if there is deep copy.
-            players.get(i).setStav(stav);
-            players.get(i).setId(i);
-            players.get(i).profiler = profiler;
-            players.get(i).init();
-        }
+		shuffleDeck();
 
-        resetMoney();
+		players.clear();
+		players.add(PlayerFactory.create(settings.value("players/front",
+				"HumanPlayer").toString()));
+		players.add(PlayerFactory.create(settings.value("players/left",
+				"RandomPlayer").toString()));
+		players.add(PlayerFactory.create(settings.value("players/right",
+				"RandomPlayer").toString()));
+		players.get(0).name = settings.value("players/front_name", "front")
+				.toString();
+		players.get(1).name = settings.value("players/left_name", "left")
+				.toString();
+		players.get(2).name = settings.value("players/right_name", "right")
+				.toString();
 
-        stav.forhont = 2;
-        stav.kolo = -6;
-    }
+		// TODO Use for-each loop.
+		for (int i = 0; i < 3; i++) {
+			// TODO May be internal objects are unchainged if there is deep
+			// copy.
+			players.get(i).setStav(stav);
+			players.get(i).setId(i);
+			players.get(i).profiler = profiler;
+			players.get(i).init();
+		}
+
+		resetMoney();
+
+		stav.forhont = 2;
+		stav.kolo = -6;
+	}
 
 	public void resetMoney() {
 		// TODO To be done by Miro ...
 	}
 
 	public void results() {
-        int p = stav.trick();
+		int p = stav.trick();
 
-        if(quickGame == false){
-            //TODO DeskView should be implemented.
-            //DeskView.log("Stich berie "+players.get(p).name);
-            LOGGER.info("Stich berie " + players.get(p).name);
-        }
+		if (quickGame == false) {
+			DeskView.log("Stich berie "+players.get(p).name);
+			LOGGER.info("Stich berie " + players.get(p).name);
+		}
 
-        for(Integer c : stav.kopa){
-            if(Card.value(c).equals("10") || Card.value(c).equals("eso")) {
-                players.get(p).body += 10;
-            }
-        }
+		for (Integer c : stav.kopa) {
+			if (Card.value(c).equals("10") || Card.value(c).equals("eso")) {
+				players.get(p).body += 10;
+			}
+		}
 
-        if(stav.kolo==9){
-            players.get(p).body += 10;
-        }
+		if (stav.kolo == 9) {
+			players.get(p).body += 10;
+		}
 
-        stav.id = p;
+		stav.id = p;
 
-        /*
-         * First kolo increase, then animation.
-         */
-        stav.kolo++;
+		/*
+		 * First kolo increase, then animation.
+		 */
+		stav.kolo++;
 
-        if(quickGame == false) {
-            //TODO DeskView should be implemented.
-            //DeskView.animateStich(p);
-        }
+		if (quickGame == false) {
+			DeskView.animateStich(p);
+		}
 
-        stav.pHist.add(p);
-        stav.vysid = p;
-        stav.kopa.clear();
+		stav.pHist.add(p);
+		stav.vysid = p;
+		stav.kopa.clear();
 
-        if(stav.kolo==10){
-            profiler.stop("game - trick taking");
-            int res = stav.results(true);
-            LOGGER.info("stav results: " + res);
-            players.get(stav.forhont).peniaze += res*2;
-            players.get((stav.forhont+1)%3).peniaze -= res;
-            players.get((stav.forhont+2)%3).peniaze -= res;
-            if(players.get(0).type=="human" && players.get(1).type=="minimax" && players.get(2).type=="smart"){
-                stats[stav.forhont] += res*2;
-                stats[(stav.forhont+1)%3] -= res;
-                stats[(stav.forhont+2)%3] -= res;
-            }
-        }
+		if (stav.kolo == 10) {
+			profiler.stop("game - trick taking");
+			int res = stav.results(true);
+			LOGGER.info("stav results: " + res);
+			players.get(stav.forhont).peniaze += res * 2;
+			players.get((stav.forhont + 1) % 3).peniaze -= res;
+			players.get((stav.forhont + 2) % 3).peniaze -= res;
+			if (players.get(0).type == "human"
+					&& players.get(1).type == "minimax"
+					&& players.get(2).type == "smart") {
+				stats[stav.forhont] += res * 2;
+				stats[(stav.forhont + 1) % 3] -= res;
+				stats[(stav.forhont + 2) % 3] -= res;
+			}
+		}
 
-        profiler.stop("trick taking - stich results");
-        if(quickGame == true) {
-            animationFinished(stav.kolo);
-        }
+		profiler.stop("trick taking - stich results");
+		if (quickGame == true) {
+			animationFinished(stav.kolo);
+		}
 	}
 
 	/**
