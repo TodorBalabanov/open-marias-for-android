04c471ba912724d070009173ea1f10f539a2218f
todor balabanov <todor.balabanov@gmail.com>
2013-08-20 13:36:35 +0300

    Bidding activity was added.

---
+++ src/eu/veldsoft/marias/BiddingActivity.java
@@ -0,0 +1,22 @@
+package eu.veldsoft.marias;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class BiddingActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_bidding);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.bidding, menu);
+		return true;
+	}
+
+}
