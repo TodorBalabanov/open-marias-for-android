837644366e64d4eb1eca6667fbd5600cb6cf1933
kalin066 <kalin066@gmail.com>
2013-08-21 10:53:53 +0300

    Work unit was completed.

--- src/eu/veldsoft/marias/GlobalStrings.java
+++ src/eu/veldsoft/marias/GlobalStrings.java
@@ -1,11 +1,43 @@
 package eu.veldsoft.marias;
 
 /**
+ * Class for global strings usage.
  * 
+ * @author Kalin Vladimirov
+ * @email kalin06_@abv.bg
+ * @date 19 Aug 2013
  */
 class GlobalStrings {
+
+	/**
+	 * 
+	 * @param f
+	 * @return
+	 * 
+	 * @author Kalin Vladimirov
+	 * @email kalin06_@abv.bg
+	 * @date 19 Aug 2013
+	 */
 	public static String flek(int f) {
-		// TODO To be done by Kalin ...
-		return (null);
+		switch (f) {
+		case 1:
+			return ("");
+		case 2:
+			return ("Flek");
+		case 4:
+			return ("Re");
+		case 8:
+			return ("Tutti");
+		case 16:
+			return ("Boty");
+		case 32:
+			return ("Kalhoty");
+		case 64:
+			return ("Kaiser");
+		case 128:
+			return ("Rekaiser");
+		default:
+			return ("?");
+		}
 	}
 }
