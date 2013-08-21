5302edb06ff873a440abac4e6e2f4279ad25ccf1
kalin066 <kalin066@gmail.com>
2013-08-21 10:53:53 +0300

    Work unit was completed.

--- src/eu/veldsoft/marias/PlayerFactory.java
+++ src/eu/veldsoft/marias/PlayerFactory.java
@@ -1,12 +1,54 @@
 package eu.veldsoft.marias;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
+ * Player factory class.
  * 
- *
+ * @author Kalin Vladimirov
+ * @email kalin06_@abv.bg
+ * @date 19 Aug 2013
  */
 class PlayerFactory {
+	/**
+	 * Logger for debug.
+	 */
+	private final static Logger LOGGER = Logger.getLogger(PlayerFactory.class
+			.getName());
+	static {
+		LOGGER.setLevel(Level.INFO);
+	}
+
+	/**
+	 * 
+	 * @param type
+	 * 
+	 * @return
+	 * 
+	 * @author Kalin Vladimirov
+	 * @email kalin06_@abv.bg
+	 * @date 19 Aug 2013
+	 */
 	public static Player create(String type) {
-		//TODO To be done by Kalin ...
-		return (null);
+		if (type.equals("HumanPlayer")) {
+			return (new HumanPlayer());
+		} else if (type.equals("RandomPlayer")) {
+			return (new RandomPlayer());
+		} else if (type.equals("MinPlayer")) {
+			return (new MinPlayer());
+		} else if (type.equals("MaxPlayer")) {
+			return (new MaxPlayer());
+		} else if (type.equals("SmartPlayer")) {
+			return (new SmartPlayer());
+		} else if (type.equals("MinimaxPlayer")) {
+			return (new MinimaxPlayer());
+		} else if (type.equals("TrustingMinimaxPlayer")) {
+			return (new TrustingMinimaxPlayer());
+		}
+
+		LOGGER.info("Invalid player type:" + type);
+
+		return (new RandomPlayer());
 	}
 }
